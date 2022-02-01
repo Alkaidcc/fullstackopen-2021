dbddf370983ca8171b2b1ffd9247b96c11746381
Alkaidcc <932214806@qq.com>
2022-02-01 17:00:13 +0800

    anecdotes: Exercises1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react'
 
+
+const MostVotes = ({points,anecdotes}) => {
+  const maxIndex = points.indexOf(Math.max(...points))
+  return (
+    <p>{anecdotes[maxIndex]}</p>
+  )
+
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -10,19 +19,29 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState([0,0,0,0,0,0,0])
 
   const handleAnecodtes = () => {
-    let random = Math.floor(Math.random() * anecdotes.length)
+    const random = Math.floor(Math.random() * anecdotes.length)
     setSelected(random)
-    console.log(random)
+  }
+  const handleVoteClick = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
   }
 
   return (
     <div>
+      <h1>Anecodte of the day</h1>
       <p>{anecdotes[selected]}</p>
+      <p>has {points[selected] } votes</p>
+      <button onClick={handleVoteClick} >vote</button>
       <button onClick={handleAnecodtes}>next anecodte</button>
+      <h1>Anecodte with most votes</h1>
+      <MostVotes points={points} anecdotes={anecdotes}  />
     </div>
   )
 }
