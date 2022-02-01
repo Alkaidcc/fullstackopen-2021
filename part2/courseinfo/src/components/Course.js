2094ddab6b7ebfaac8cea96582ea0ab869b9c3d0
Alkaidcc <932214806@qq.com>
2022-02-01 20:10:24 +0800

    courseinfo: Exercises2.2-2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,6 +1,6 @@
 const Header = ({name}) => {
     return (
-        <h1>{name}</h1>
+        <h2>{name}</h2>
     )
 }
 
@@ -14,7 +14,12 @@ const Content = ({content}) => {
 
 const Part = ({name,exercises}) => <p>{name} {exercises}</p>
 
-
+const Total = ({parts}) => {
+    const num = parts.reduce((acc,part) => acc + part.exercises,0)
+    return (
+        <b>Total of {num} exercises</b>
+    )
+}
 const Course = (props) => {
     const { course } = props
 
@@ -22,6 +27,7 @@ const Course = (props) => {
         <>
             <Header name={course.name} />
             <Content content={course.parts}/>
+            <Total parts={course.parts}/>
         </>
     )
 }
