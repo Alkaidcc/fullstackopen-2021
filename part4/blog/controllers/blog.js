26648b381eef7cffe91f48f3ff63d63e5bc332b9
Alkaidcc <932214806@qq.com>
2022-02-09 16:44:33 +0800

    blog: Exercises4.3-4.7

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -2,53 +2,53 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (req, res) => {
-    Blog.find({})
-        .then(blogs => {
-            res.json(blogs)
-        })
+  Blog.find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
 })
 
 blogsRouter.get('/:id', (req, res, next) => {
-    Blog.findById(req.params.id)
-        .then(blog => {
-            if (blog) {
-                res.json(blog)
-            } else {
-                res.status(404).send({ error: 'blog not found' })
-            }
-        })
-        .catch(error => next(error))
+  Blog.findById(req.params.id)
+    .then(blog => {
+      if (blog) {
+        res.json(blog)
+      } else {
+        res.status(404).send({ error: 'blog not found' })
+      }
+    })
+    .catch(error => next(error))
 })
 
 blogsRouter.delete('/:id', (req, res, next) => {
-    Blog.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).json(result)
-        })
-        .catch(error => next(error))
+  Blog.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).json(result)
+    })
+    .catch(error => next(error))
 })
 
 blogsRouter.put('/:id', (req, res, next) => {
-    const body = req.body
-    const blog = {
-        name: body.name,
-        number: body.number
-    }
-    Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  const body = req.body
+  const blog = {
+    name: body.name,
+    number: body.number
+  }
+  Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 blogsRouter.post('/',(req,res,next)=>{
 
-    const blog = new Blog(req.body)
-    blog.save()
-        .then(savedBlog => {
-            res.status(201).json(savedBlog)
-        })
-        .catch(error => next(error))
+  const blog = new Blog(req.body)
+  blog.save()
+    .then(savedBlog => {
+      res.status(201).json(savedBlog)
+    })
+    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
